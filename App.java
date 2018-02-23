217e3e533c50905ec5089d710222b93b651eb348
Prathyuus <ptushatata@gmail.com>
2018-02-23 15:40:41 +0530

    added 5 files

---
+++ App.java
@@ -0,0 +1,13 @@
+package Sapi.Razo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
